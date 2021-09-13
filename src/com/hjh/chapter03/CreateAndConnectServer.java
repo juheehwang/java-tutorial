3258f716d6cfbc60a4018ed448d0b10d4acecc83
hwangjuhui <hjh@drmamma.co.kr>
2021-09-13 16:29:23 +0900

    Server complete and client start

--- src/com/hjh/chapter03/CreateAndConnectServer.java
+++ src/com/hjh/chapter03/CreateAndConnectServer.java
@@ -1,16 +1,31 @@
 package com.hjh.chapter03;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class CreateAndConnectServer {
     public static void main(String[] args) {
 
-        try {
-            ServerSocket serverSocket = new ServerSocket(9999);
-            
+        try (ServerSocket serverSocket = new ServerSocket(9999)) {
+            while (true) {
+                Socket request = serverSocket.accept();
+                System.out.println("server open" + serverSocket.accept().getInetAddress().getHostName());
+
+                var in = new BufferedReader(new InputStreamReader(request.getInputStream()));
+                String message = in.readLine();
+                System.out.println("msg from Client" + message);
+
+                var out = new BufferedWriter(new OutputStreamWriter(request.getOutputStream()));
+                out.write(message + "\n");
+                out.flush();
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+
         }
+
+
     }
 }
---
+++ src/com/hjh/chapter03/CreateAndConnectionClient.java
@@ -0,0 +1,7 @@
+package com.hjh.chapter03;
+
+public class CreateAndConnectionClient {
+    public static void main(String[] args) {
+        
+    }
+}
