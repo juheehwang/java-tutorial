15382d042eb6b87f738c8186223ba759de65864e
hwangjuhui <hjh@drmamma.co.kr>
2021-09-15 00:28:14 +0900

    connection sever continuously

--- src/com/hjh/chapter03/CreateAndConnectServer.java
+++ src/com/hjh/chapter03/CreateAndConnectServer.java
@@ -1,38 +1,1 @@
-package com.hjh.chapter03;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class CreateAndConnectServer {
-    public static void main(String[] args) {
-        BufferedReader in;
-        //   BufferedWriter out = null;
-        Socket socket;
-
-        try (ServerSocket serverSocket = new ServerSocket(9999)) {
-            socket = serverSocket.accept();
-            System.out.println("server connected by client");
-            System.out.println("server port" + socket.getInetAddress());
-
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            // out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            while (true) {
-                String message = in.readLine();
-
-                if (message == null) {
-                    System.out.println("server is closed by client");
-                    break;
-                }
-                System.out.println("msg from Client: " + message + "\r");
-            }
-//            out.write("여기는 서버에서 보낸당");
-//            out.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+package com.hjh.chapter03;import java.io.*;import java.net.ServerSocket;import java.util.Scanner;public class CreateAndConnectServer {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        try (ServerSocket serverSocket = new ServerSocket(9999)) {            while (true) {                var socket = serverSocket.accept();                System.out.println("server connected by client");                System.out.println("server port" + socket.getInetAddress());                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));//                    System.out.print("클라이언트에 회신할 말? ");//                    String serverSendMsg = sc.nextLine();                if (socket.isBound()) {                    while (true) {                        String message = in.readLine();                        System.out.println("msg from Client: " + message);                        out.write(message + "\r\n");                        out.flush();                        if (message.equals("exit") || message == null) {                            System.out.println("server is closed by client");                            break;                        }                    }                }            }        } catch (IOException e) {            e.printStackTrace();        }    }}
--- src/com/hjh/chapter03/CreateAndConnectionClient.java
+++ src/com/hjh/chapter03/CreateAndConnectionClient.java
@@ -1,39 +1,1 @@
-package com.hjh.chapter03;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-import java.util.Scanner;
-
-public class CreateAndConnectionClient {
-
-    public static void main(String[] args) {
-        BufferedWriter out = null;
-        // BufferedReader in = null;
-
-        try (Scanner sc = new Scanner(System.in); Socket clientSocket = new Socket("127.0.0.1", 9999)) {
-            //in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-            while (true) {
-                System.out.print("글자를 입력해주세요!!");
-                String clientMsg = sc.nextLine();
-
-                if (clientMsg.equals("exit")) {
-                    out.write(clientMsg);
-                    break;
-                }
-
-                out.write(clientMsg + "\n");
-                out.flush();
-
-            }
-//            String serverMsg = in.readLine();
-//            System.out.println("!!serverMessage!!" + serverMsg);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+package com.hjh.chapter03;import java.io.*;import java.net.Socket;import java.util.Scanner;public class CreateAndConnectionClient {    public static void main(String[] args) {        try (Scanner sc = new Scanner(System.in);             Socket clientSocket = new Socket("127.0.0.1", 9999)        ) {            var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));            var out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));            while (true) {                System.out.print("글자를 입력해주세요!!");                String clientMsg = sc.nextLine();                out.write(clientMsg + "\r");                out.flush();                if (clientMsg.equals("exit")) {                    System.out.println("서버와의 1 연결이 종료되었습니다.");                    break;                }                String serverMsg = in.readLine();                System.out.println("서버로부터 온 메세지: " + serverMsg + "\r");//                if (serverMsg == null || serverMsg.equals("exit")) {//                    System.out.println("서버와의 2 연결이 종료되었습니다.");//                    break;//                }            }        } catch (IOException e) {            System.out.println("여기오나?");            e.printStackTrace();        }    }}
