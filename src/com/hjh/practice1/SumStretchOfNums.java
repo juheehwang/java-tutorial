a337e3a2e5cb35b1d386589fa612cf5074205e80
juhee hwang <hjh@drmamma.co.kr>
2022-08-11 14:59:03 +0900

    practice 3 - sum between start index and end index

---
+++ src/com/hjh/practice1/SumStretchOfNums.java
@@ -0,0 +1,26 @@
+package com.hjh.practice1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SumStretchOfNums {
+
+  private final static String sampleString = "12345";
+  private final static int sampleInt = 1234;
+
+
+  public static void main(String[] args) {
+    int[] numList = new int[]{2,1,4,5,6,9,3};
+    sumNumsBetweenStartToEndIndex(numList,4,4);
+  }
+
+
+  public static void sumNumsBetweenStartToEndIndex(int[] numList,int start,int end){
+
+   int result = 0;
+   for(int i = start-1; i<end;i++){
+     result +=numList[i];
+   }
+    System.out.println(result);
+  }
+}
